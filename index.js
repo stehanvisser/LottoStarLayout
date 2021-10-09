c9253cd7dd273e2c106c95fd015fd6ab8f9c0029
stehan <stehan@fastcomm.com>
2021-10-09 19:53:11 +0200

    submitted form view added

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
-function validateForm() {}
+const loginUrl = "https://lottostar.co.za/login";
+const registrationUrl = "https://lottostar.co.za/register";
+
+function validateForm(e) {
+  e.preventDefault();
+  let mainForm = document.getElementById("main-form");
+  let submittedForm = document.getElementById("form-submitted");
+  mainForm.style.display = "none";
+  submittedForm.style.display = "block";
+}
+
+function login() {
+  window.open(loginUrl, "_blank").focus();
+}
+
+function register() {
+  window.open(registrationUrl, "_blank").focus();
+}
